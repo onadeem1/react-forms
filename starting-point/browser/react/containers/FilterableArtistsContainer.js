6ea342702ff6102126feb1be5b6e84f10bb1a48d
Omer Nadeem <omernadeem3@gmail.com>
2017-02-08 18:16:39 -0500

    artist form works

---
+++ starting-point/browser/react/containers/FilterableArtistsContainer.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import FilterInput from '../components/FilterInput'
+import Artists from '../components/Artists'
+
+
+export default class FilterableArtistsContainer extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {inputValue: ''}
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (evt) {
+    const value = evt.target.value;
+    this.setState({
+      inputValue: value
+    });
+  }
+
+  render() {
+    const inputValue = this.state.inputValue;
+    const filteredArtists = this.props.artists.filter(artist => artist.name.match(inputValue));
+
+    return (
+      <div>
+        <FilterInput handleChange={this.handleChange} />
+        <Artists artists={filteredArtists} />
+      </div>
+    )
+  }
+}
