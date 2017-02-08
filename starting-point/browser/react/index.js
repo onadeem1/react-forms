5549dd5e2c8bf21c888f2d9d8ac04d60ce4910bc
Omer Nadeem <omernadeem3@gmail.com>
2017-02-08 18:30:51 -0500

    playlist

--- starting-point/browser/react/index.js
+++ starting-point/browser/react/index.js
@@ -8,6 +8,7 @@ import Artists from './components/Artists';
 import Artist from './components/Artist';
 import Songs from './components/Songs';
 import FilterableArtistsContainer from './containers/FilterableArtistsContainer'
+import NewPlaylist from './components/NewPlaylist';
 
 ReactDOM.render(
   <Router history={hashHistory}>
@@ -15,6 +16,7 @@ ReactDOM.render(
       <Route path="/albums" component={Albums} />
       <Route path="/albums/:albumId" component={Album} />
       <Route path="/artists" component={FilterableArtistsContainer} />
+      <Route path="/playlist" component={NewPlaylist} />
       <Route path="/artists/:artistId" component={Artist}>
         <Route path="/artists/:artistId/albums" component={Albums} />
         <Route path="/artists/:artistId/songs" component={Songs} />
